5fe7f638a3f911804ad8204270849d404953a1f5
KosmX <kosmx.mc@gmail.com>
2022-05-13 19:36:32 +0200

    some stuff

---
+++ src/main/kotlin/dev/kosmx/emoteFormatCLI/Main.kt
@@ -0,0 +1,28 @@
+package dev.kosmx.emoteFormatCLI
+
+import dev.kosmx.emoteFormatCLI.executorImpl.Executor
+import io.github.kosmx.emotes.executor.EmoteInstance
+import org.apache.commons.cli.Option
+import org.apache.commons.cli.Options
+
+val progDescription = """Emotes Format Tools CLI
+    Tools to convert emotecraft format from and to.
+Args:
+""".trimIndent()
+
+fun main() {
+    val options = Options()
+    val input = Option("i", "input", true ,"The array input, \"-\" if the input will be received in the stdin, \"*\" if passed as an \$arg")
+    val output = Option("o", "output", true, "The result array, \"-\" if use stdout")
+    val mode = Option("m", "mode", true, "Operation Mode, see help for more info")
+    val help = Option("h", "help", false, "Print the help text")
+    val arg = Option("a", "arg", true, "the input json if used -i *")
+    val input2 = Option("i2", "input2", true, "Secondary input, see modes")
+
+
+
+
+    EmoteInstance.instance = Executor()
+
+
+}
---
+++ src/main/kotlin/dev/kosmx/emoteFormatCLI/executorImpl/Executor.kt
@@ -0,0 +1,35 @@
+package dev.kosmx.emoteFormatCLI.executorImpl
+
+import io.github.kosmx.emotes.executor.EmoteInstance
+import io.github.kosmx.emotes.executor.Logger
+import io.github.kosmx.emotes.executor.dataTypes.IClientMethods
+import io.github.kosmx.emotes.executor.dataTypes.IDefaultTypes
+import io.github.kosmx.emotes.executor.dataTypes.IGetters
+import java.nio.file.Path
+import java.util.logging.Level
+
+class Executor: EmoteInstance() {
+    override fun getLogger(): Logger {
+        return Logger() { _: Level, _: String -> /* nothing */ }
+    }
+
+    override fun getDefaults(): IDefaultTypes {
+        throw java.lang.UnsupportedOperationException("This is not a game instance!")
+    }
+
+    override fun getGetters(): IGetters {
+        throw java.lang.UnsupportedOperationException("This is not a game instance!")
+    }
+
+    override fun getClientMethods(): IClientMethods {
+        throw java.lang.UnsupportedOperationException("This is not a game instance!")
+    }
+
+    override fun isClient(): Boolean {
+        throw java.lang.UnsupportedOperationException("This is not a game instance!")
+    }
+
+    override fun getGameDirectory(): Path {
+        throw java.lang.UnsupportedOperationException("This is not a game instance!")
+    }
+}
